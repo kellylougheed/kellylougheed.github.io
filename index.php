6f32bd0082db5fae85928b2e897d46d0c18898c9
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-08 11:59:21 -0700

    Sunset layout

---
+++ index.php
@@ -0,0 +1,112 @@
+<meta name="description" content="Web developer Kelly Lougheed's portfolio of web applications made with HTML, CSS, JavaScript, jQuery, PHP, and Ruby on Rails.">
+
+<?php print file_get_contents("header.html")?>
+
+	<a name="projects"></a>
+
+  	<div class="title">Projects</div>
+
+		<div class="content">
+
+			<div class="container">
+
+				<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.kellylougheed.com/weather-app" target="_blank"><img src="http://www.kellylougheed.com/images/projects/weatherapp.png" class="responsive" alt="JavaScript weather app with APIs"></a>
+				<p><span class="title">Weather App</span><br>
+				JavaScript • jQuery • HTML • CSS<br>
+				<span class="descr">Reports local weather using APIs for location and weather.</span></p>
+				</div>
+
+				<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.kellylougheed.com/this-is-just-to-say" target="_blank"><img src="http://www.kellylougheed.com/images/projects/thisisjusttosay.png" class="responsive" alt="PHP poetry-writing app"></a>
+				<p><span class="title">This is Just To Say</span><br>
+				PHP • JavaScript • jQuery • HTML • CSS<br>
+				<span class="descr">Writes and mails a poem to apologize for eating your roommate's food.</span></p>
+				</div>
+
+				<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://thinkyouknowlatin.herokuapp.com" target="_blank"><img src="http://www.kellylougheed.com/images/projects/latin.png" class="responsive" alt="Ruby on Rails Latin app"></a>
+				<p><span class="title">So You Think You Know Latin</span><br>
+				Ruby on Rails • HTML • CSS • Bootstrap<br>
+				<span class="descr">Teacher tool that generates exercises for Latin class.</span></p>
+				</div>
+
+				<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://writeyourthesis.herokuapp.com" target="_blank"><img src="http://www.kellylougheed.com/images/projects/writeyourthesis.png" class="responsive" alt="Ruby on Rails app for places to write your thesis in Providence, RI"></a>
+				<p><span class="title">Write Your Thesis</span><br>
+				Ruby on Rails • HTML • CSS • Bootstrap<br>
+				<span class="descr">User-powered database of places to write your thesis (or study) in Providence, RI.</span></p>
+				</div>
+
+				<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.kellylougheed.com/calculator" target="_blank"><img src="http://www.kellylougheed.com/images/projects/calculator.png" class="responsive" alt="JavaScript calculator"></a>
+				<p><span class="title">Calculator</span><br>
+				JavaScript • jQuery • HTML • CSS<br>
+				<span class="descr">Uses order of operations to solve equations.</span></p>
+				</div>
+
+				<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.kellylougheed.com/roman/practice" target="_blank"><img src="http://www.kellylougheed.com/images/projects/romannumeral.png" class="responsive" alt="JavaScript Roman numeral app"></a>
+				<p><span class="title">Roman Numeral Practice</span><br>
+				JavaScript • jQuery • HTML • CSS<br>
+				<span class="descr">Quizzes user on converting Arabic numerals to Roman numerals.</span></p>
+				</div>
+
+				<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.kellylougheed.com/sortes" target="_blank"><img src="http://www.kellylougheed.com/images/projects/sortesvirgilianae.png" class="responsive" alt="JavaScript random quote generator"></a>
+				<p><span class="title">Fortune Teller</span><br>
+				JavaScript • jQuery • HTML • CSS<br>
+				<span class="descr">Generates a random quote from Vergil's <i>Aeneid</i> that predicts your future.</span></p>
+				</div>
+
+					<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.kellylougheed.com/wikipedia-viewer" target="_blank"><img src="http://www.kellylougheed.com/images/projects/wikipediaviewer.png" class="responsive" alt="Wikipedia Viewer made with API"></a>
+				<p><span class="title">Wikipedia Viewer</span><br>
+				JavaScript • jQuery • HTML • CSS<br>
+				<span class="descr">Searches Wikipedia and shows resulting entries using Wikipedia API.</span></p>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.kellylougheed.com/comment-writer" target="_blank"><img src="http://www.kellylougheed.com/images/projects/commentwriter.png" class="responsive" alt="JavaScript app to write student comments"></a>
+				<p><span class="title">Comment Writer</span><br>
+				JavaScript • HTML • CSS<br>
+				<span class="descr">Teacher tool that generates comments about student performance.</span></p>
+				</div>
+
+
+				<div class="col-lg-4 col-md-6 col-sm-12">
+				<a href="http://www.kellylougheed.com/email-home" target="_blank"><img src="http://www.kellylougheed.com/images/projects/emailhome.png" class="responsive" alt="JavaScript app to send emails home to parents"></a>
+				<p><span class="title">Email Home</span><br>
+				JavaScript • HTML • CSS<br>
+				<span class="descr">Teacher tool that generates emails to send home to parents of struggling students.</span></p>
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="clear"></div>
+
+		<a name="books"></a>
+
+		<div class="title">Books</div>
+
+			<div class="content">
+
+				<div class="col-lg-3 col-md-3 col-sm-6">
+				<a href="http://www.mundania.com/book.php?title=Cads+and+Cadavers" target=_"blank"><img src="http://www.kellylougheed.com/images/Cads_and_Cadavers.jpg" class="responsive" alt="Cads and Cadavers by Kelly Lougheed"></a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6">
+				<a href="http://www.mundania.com/book.php?title=The+Graveyard+Gymnasium" target=_"blank"><img src="http://www.kellylougheed.com/images/The_Graveyard_Gymnasium.jpg" class="responsive" alt="The Graveyard Gymnasium by Kelly Lougheed"></a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6">
+				<a href="http://www.mundania.com/book.php?title=Dormitory+of+the+Dead" target=_"blank"><img src="http://www.kellylougheed.com/images/Dormitory_of_the_Dead.jpg" class="responsive" alt="Dormitory of the Dead by Kelly Lougheed"></a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6">
+				<a href="http://www.mundania.com/book.php?title=Vampire+Voyage"><img src="http://www.kellylougheed.com/images/Vampire_Voyage.jpg" class="responsive" alt="Vampire Voyage by Kelly Lougheed"></a>
+
+			</div>
+
+		<div class="clear"></div>
+
+<?php print file_get_contents("footer.html")?>
