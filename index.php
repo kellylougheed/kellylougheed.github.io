e1818046020c427efbbc6658d250d2be578f4d6d
Kelly Lougheed <kellylougheed@gmail.com>
2017-05-31 09:51:08 -0700

    Add skills and teaching section

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+  <!-- Open Graph tags for LinkedIn sharing -->
+  <meta property="og:title" content="Portfolio Website" />
+  <meta property="og:description" content="Software and teaching portfolio of Kelly Lougheed" />
+  <meta property="og:url" content="http://www.kellylougheed.com" />
+  <meta property="og:image" content="http://www.kellylougheed.com/images/projects/participation.png" />
   <title>Kelly Lougheed</title>
-  <script src="https://use.fontawesome.com/ee14bb1f23.js"></script>
+  <!-- CSS -->
   <link href="style.css" rel="stylesheet" type="text/css">
+  <!-- Icons -->
+  <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
+  <script src="https://use.fontawesome.com/ee14bb1f23.js"></script>
+  <!-- Fonts -->
   <link href='https://fonts.googleapis.com/css?family=Sacramento' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 </head>
@@ -20,7 +29,7 @@
     <div class="down"><a onclick="$('#work').animatescroll();"><i class="fa fa-angle-down" aria-hidden="true"></i></a></div>
   </div><!-- header -->
 <div id="work">
-  <h1>Work</h1>
+  <h1>Projects</h1>
   <div class="container"> 
 
     <div class="col-lg-6 col-md-6 col-sm-12">
@@ -84,6 +93,76 @@
   </div><!-- container -->
 </div><!-- work -->
 
+<h1>Skills</h1>
+
+<div id="skills" class="container">
+  <div class="col-lg-1 col-md-2 col-sm-3">
+    <i class="devicon-python-plain colored"></i>
+    <br/><br/>
+    Python
+  </div>
+  <div class="col-lg-1 col-md-2 col-sm-3">
+    <i class="devicon-java-plain colored"></i>
+    <br/><br/>
+    Java
+  </div>
+  <div class="col-lg-1 col-md-2 col-sm-3">
+    <i class="devicon-ruby-plain colored"></i>
+    <br/><br/>
+    Ruby
+  </div>
+  <div class="col-lg-1 col-md-2 col-sm-3">
+    <i class="devicon-rails-plain colored"></i>
+    <br/><br/>
+    Rails
+  </div>
+  <div class="col-lg-1 col-md-2 col-sm-3">
+    <i class="devicon-php-plain colored"></i>
+    <br/><br/>
+    PHP
+  </div>
+  <div class="col-lg-1 col-md-2 col-sm-3">
+    <i class="devicon-javascript-plain colored"></i> 
+    <br/><br/>
+    JavaScript
+  </div>
+  <div class="col-lg-1 col-md-2 col-sm-3">
+    <i class="devicon-html5-plain colored"></i>
+    <br/><br/>
+    HTML5
+  </div>
+  <div class="col-lg-1 col-md-2 col-sm-3">
+    <i class="devicon-css3-plain colored"></i>
+    <br/><br/>
+    CSS3
+  </div>
+  <div class="col-lg-1 col-md-2 col-sm-3">
+    <i class="devicon-swift-plain colored"></i>
+    <br/><br/>
+    Swift
+  </div>
+</div>
+
+<h1>Teaching</h1>
+  <div class="container"> 
+    <div class="col-lg-6 col-md-6 col-sm-12">
+      <a href="http://www.kellylougheed.com/teaching" target="_blank"><img src="http://www.kellylougheed.com/images/projects/teaching.png" class="responsive" alt="Computer science and Latin teaching portfolio"></a>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-12">
+      <h2>Teaching Portfolio</h2>
+      <br/>
+      <p class="about">I'm a computer science teacher with previous experience as a Latin teacher. My teaching portfolio includes...</p>
+      <ul>
+        <li> Sample lessons and projects in computer science and Latin.</li>
+        <li> Student reviews from two institutions.</li>
+        <li> Breakdown of my education in computer science and Latin.</li>
+        <li> Description of my teaching experience at multiple schools and organizations.</li>
+        <li> Website design inspired by the computer terminal and code text editors. Code available on <a href="https://github.com/kellylougheed/teaching-portfolio" target="_blank">GitHub</a>.</li>
+        <li> Built with HTML/CSS, Bootstrap, and PHP.</li>
+      </ul>
+    </div>
+</div>
+
 <h1>About</h1>
 <div class="container">
   <div class="col-lg-3 col-md-4 col-sm-12">
@@ -103,7 +182,10 @@
 </div>
 
 <div class="footer">
-  <p>Made with <a href="http://www.philzcoffee.com/" target="_blank"><i class="fa fa-coffee" aria-hidden="true"></i></a> and <a href="https://www.google.com/search?q=weather%20los%20angeles%20ca" target="_blank"><i class="fa fa-sun-o" aria-hidden="true"></i></a> by Kelly Lougheed, 2016-17.</p>
+  <p>
+    Made with <a href="http://www.philzcoffee.com/" target="_blank"><i class="fa fa-coffee" aria-hidden="true"></i></a> and <a href="https://www.google.com/search?q=weather%20los%20angeles%20ca" target="_blank"><i class="fa fa-sun-o" aria-hidden="true"></i></a> by Kelly Lougheed, 2016-17.
+    <a href="https://github.com/kellylougheed/portfolio" target="_blank"><i class="fa fa-code" aria-hidden="true"></i></a>
+  </p>
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
