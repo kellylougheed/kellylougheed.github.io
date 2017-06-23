6ba29ba0c5118e3dd31dfad89787e89e6bbc6112
Kelly Lougheed <kellylougheed@gmail.com>
2017-06-23 12:02:03 -0700

    New design for tools and links

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     <div class="down"><a onclick="$('#work').animatescroll();"><i class="fa fa-angle-down" aria-hidden="true"></i></a></div>
   </div><!-- header -->
 <div id="work">
-  <h1>Projects</h1>
+  <h1>Work</h1>
 
   <div class="container"> 
 
@@ -39,108 +39,106 @@
 
     <div class="col-lg-6 col-md-6 col-sm-12">
       <h2>Participation</h2>
+      <p class="tools">
+        <span>Ruby on Rails</span>
+        <span>JavaScript</span>
+        <span>TDD</span>
+        <span>Bootstrap</span>
+        <span>HTML</span>
+        <span>CSS</span>
+      </p>
       <ul>
         <li> Gradebook app for high school teachers.</li>
         <li> Teachers can input their rosters and track participation points for each student.</li>
         <li> Teachers can record comments about students to use in report cards &amp; parent-teacher conferences.</li>
-        <li> Built with Ruby on Rails, JavaScript, HTML/CSS. Security managed by Devise.</li>
-        <li> The code is <a href="https://github.com/kellylougheed/participation" target="_blank">open-source on GitHub</a> and has >95% test coverage. </li>
-        <li> Read the blog post about the story and philosophy behind the app, <a href="http://kellylougheed.com/blog/participation-points-real/" target="_blank">Your Participation Points Are Real</a>. </li>
+        <li> Comments can be automatically emailed to students.</li>
+        <li> The app was built with TDD and has >95% test coverage. </li>
       </ul>
+      <p class="proj-links">
+        <a href="http://participation.herokuapp.com" target="_blank">Live Site</a>
+        <a href="https://github.com/kellylougheed/participation" target="_blank">Github</a>
+        <a href="http://kellylougheed.com/blog/participation-points-real/" target="_blank">Blog Post</a>
+      </p>
     </div>
 
     <div class="col-lg-6 col-md-6 col-sm-12">
       <br/>
       <h2>Roman Numeral Quiz</h2>
+      <p class="tools">
+        <span>AngularJS</span>
+        <span>JavaScript</span>
+        <span>HTML</span>
+        <span>CSS</span>
+      </p>
       <ul>
         <li> App to quiz students on Roman numerals.</li>
         <li> Students convert Arabic numerals to Roman numerals and receive instant feedback.</li>
         <li> The app tracks the student's history of correct and incorrect answers.</li>
-        <li> Includes suggestions for use in the classroom.</li>
-        <li> Built with AngularJS, JavaScript, and HTML/CSS.</li>
-        <li> The code is <a href="https://github.com/kellylougheed/roman-numeral-quiz" target="_blank">open-source on GitHub</a>.</li>
-        <li> Read the blog post converting the app from vanilla JavaScript and jQuery to the AngularJS framework: <a href="http://kellylougheed.com/blog/angularjs-ever-done/" target="_blank">What Has AngularJS Ever Done For Me?</a> </li>
+        <li> Includes teacher section with suggestions for classroom use in Latin or math class.</li>
       </ul>
+      <p class="proj-links">
+        <a href="http://www.kellylougheed.com/roman" target="_blank">Live Site</a>
+        <a href="https://github.com/kellylougheed/roman-numeral-quiz" target="_blank">Github</a>
+        <a href="http://kellylougheed.com/blog/angularjs-ever-done/" target="_blank">Blog Post</a>
+      </p>
     </div>
 
     <div class="col-lg-6 col-md-6 col-sm-12">
       <a href="http://www.kellylougheed.com/roman" target="_blank"><img src="http://www.kellylougheed.com/images/projects/romannumeral.png" class="responsive" alt="AngularJS Roman numeral quiz app"></a>
     </div>
 
-    <div class="col-lg-4 col-md-6 col-sm-12">
-      <a href="http://device-o.herokuapp.com" target="_blank"><img src="http://www.kellylougheed.com/images/projects/deviceo.png" class="responsive" alt="Rails game to quiz students on literary devices"></a>
-      <p><span class="title">Device-O!</span><br/>
-      Ruby on Rails • JavaScript • HTML • CSS • Bootstrap<br/>
-      <span class="descr">Game to quiz students on literary devices for AP English or AP Latin.</span><br/>
-      <span class="links"><a href="http://kellylougheed.com/blog/app-takes-english-class-seriously/" target="_blank"><i class="fa fa-wordpress" aria-hidden="true"></i></a>&nbsp;&nbsp;<a href="https://github.com/kellylougheed/device-o" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></p>
-    </div>
-
-    <div class="col-lg-4 col-md-6 col-sm-12">
-      <a href="http://www.kellylougheed.com/weather-app" target="_blank"><img src="http://www.kellylougheed.com/images/projects/weatherapp.png" class="responsive" alt="JavaScript app that consumes weather API"></a>
-      <p><span class="title">Weather App</span><br/>
-      JavaScript • HTML • CSS<br/>
-      <span class="descr">Weather app that consumes APIs for location and weather.</span><br/>
-      <span class="links"><a href="https://github.com/kellylougheed/weather-app" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></p>
-    </div>
-
-    <div class="col-lg-4 col-md-6 col-sm-12">
+    <div class="col-lg-6 col-md-6 col-sm-12">
       <a href="http://www.kellylougheed.com/this-is-just-to-say" target="_blank"><img src="http://www.kellylougheed.com/images/projects/thisisjusttosay.png" class="responsive" alt="PHP poetry-writing app"></a>
-      <p><span class="title">This is Just To Say</span><br/>
-      PHP • JavaScript • HTML • CSS<br/>
-      <span class="descr">Writes and mails a poem to apologize for eating your roommate's food.</span><br/>
-      <span class="links"><a href="http://kellylougheed.com/blog/says-computers-cant-write-poetry/" target="_blank"><i class="fa fa-wordpress" aria-hidden="true"></i></a>&nbsp;&nbsp;<a href="https://github.com/kellylougheed/this-is-just-to-say" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></p>
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-12">
+      <br/>
+      <h2>This Is Just To Say</h2>
+      <p class="tools">
+        <span>PHP</span>
+        <span>JavaScript</span>
+        <span>HTML</span>
+        <span>CSS</span>
+      </p>
+      <ul>
+        <li> Generates a poem to apologize for eating your roommate's food.</li>
+        <li> Emails the poem to a recipient of your choice.</li>
+        <li> Can be used in the English classroom to teach William Carlos Williams.</li>
+      </ul>
+      <p class="proj-links">
+        <a href="http://www.kellylougheed.com/this-is-just-to-say" target="_blank">Live Site</a>
+        <a href="https://github.com/kellylougheed/this-is-just-to-say" target="_blank">Github</a>
+        <a href="http://kellylougheed.com/blog/says-computers-cant-write-poetry/" target="_blank">Blog Post</a>
+      </p>
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-12">
+      <br/>
+      <h2>Teaching Portfolio</h2>
+      <p class="tools">
+        <span>PHP</span>
+        <span>Bootstrap</span>
+        <span>HTML</span>
+        <span>CSS</span>
+      </p>
+      <ul>
+        <li> Sample lessons and projects in Computer Science and Latin.</li>
+        <li> Narratives of my teaching experience and education in Computer Science and Latin.</li>
+        <li> Student reviews.</li>
+        <li> Website design inspired by the computer terminal and code text editors.</li>
+      </ul>
+      <p class="proj-links">
+        <a href="http://www.kellylougheed.com/teaching" target="_blank">Live Site</a>
+        <a href="https://github.com/kellylougheed/teaching-portfolio" target="_blank">Github</a>
+      </p>
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-12">
+      <a href="http://www.kellylougheed.com/teaching" target="_blank"><img src="http://www.kellylougheed.com/images/projects/teaching.png" class="responsive" alt="Computer science and Latin teaching portfolio"></a>
     </div>
 
   </div><!-- container -->
 </div><!-- work -->
-
-<?php 
-$skills = array(
-  "Python" => "devicon-python-plain",
-  "Java" => "devicon-java-plain",
-  "Ruby" => "devicon-ruby-plain",
-  "Rails" => "devicon-rails-plain",
-  "PHP" => "devicon-php-plain",
-  "JavaScript" => "devicon-javascript-plain",
-  "HTML5" => "devicon-html5-plain",
-  "CSS3" => "devicon-css3-plain",
-  "Swift" => "devicon-swift-plain",
-  "PhotoShop" => "devicon-photoshop-plain"
-  );
-?>
-
-<h1>Skills</h1>
-
-<div id="skills" class="container">
-<?php
-foreach ($skills as $name => $class) {
-  echo '<div class="col-lg-1 col-md-2 col-sm-3">';
-  echo '<i class="' . $class . ' colored"></i><br/><br/>';
-  echo $name;
-  echo '</div>';
-}
-?>
-</div>
-
-<h1>Teaching</h1>
-  <div class="container"> 
-    <div class="col-lg-6 col-md-6 col-sm-12">
-      <a href="http://www.kellylougheed.com/teaching" target="_blank"><img src="http://www.kellylougheed.com/images/projects/teaching.png" class="responsive" alt="Computer science and Latin teaching portfolio"></a>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-12">
-      <h2>Teaching Portfolio</h2>
-      <br/>
-      <p class="about">My teaching portfolio includes...</p>
-      <ul>
-        <li> Sample lessons and projects in Computer Science and Latin.</li>
-        <li> Student reviews from two institutions.</li>
-        <li> Breakdown of my education in Computer Science and Latin.</li>
-        <li> Description of my teaching experience at multiple schools and organizations.</li>
-        <li> Website design inspired by the computer terminal and code text editors. Code available on <a href="https://github.com/kellylougheed/teaching-portfolio" target="_blank">GitHub</a>.</li>
-        <li> Built with PHP, HTML/CSS, and Bootstrap.</li>
-      </ul>
-    </div>
-</div>
 
 <h1>About</h1>
 <div class="container">
@@ -149,15 +147,41 @@ foreach ($skills as $name => $class) {
   </div>
   <div class="col-lg-9 col-md-8 col-sm-12">
     <p class="about">
-    	I'm a Los Angeles-based computer science teacher. I started making websites to advertise <a href="https://www.goodreads.com/author/show/4486062.Kelly_Lougheed" target="_blank">my writing</a>, and now I build full-stack web applications. I believe that computer science has a place in every student's education and that the CS skillset can be applied to any field.
+    	I'm a Los Angeles-based Computer Science teacher. I started making websites to advertise <a href="https://www.goodreads.com/author/show/4486062.Kelly_Lougheed" target="_blank">my writing</a>, and now I build full-stack web applications. As a Classics (Latin &amp; Greek) graduate of Brown University and former Latin teacher, I particularly enjoy building applications that relate to education and the humanities.
     </p>
     <p class="about">
-      I have worked as a software developer using JavaScript, Python, and PHP and also enjoy building projects in Ruby on Rails and Swift. Most recently, I've been pursing a certificate in Computer Science Education at the College of St. Scholastica.
+      I have worked as a software developer using JavaScript, Python, and PHP and have also built apps in Ruby on Rails and Swift. Most recently, I've been pursing a certificate in Computer Science Education at the College of St. Scholastica.
     </p>
     <p class="about">
-      As a Classics (Latin &amp; Greek) graduate of Brown University and former Latin teacher, I particularly enjoy building projects that relating to Latin, Italian, literature, and education.
+      I believe that computer science has a place in every student's education and that the CS skillset can be applied to any field. Beyond programming, I'm also interested in technology history, digital humanities, and the social and ethical issues surrounding tech.
     </p>
   </div>
+</div>
+
+<?php 
+$skills = array(
+  "JavaScript" => "devicon-javascript-plain",
+  "Ruby" => "devicon-ruby-plain",
+  "Rails" => "devicon-rails-plain",
+  "PHP" => "devicon-php-plain",
+  "Python" => "devicon-python-plain",
+  "Java" => "devicon-java-plain",
+  "Swift" => "devicon-swift-plain",
+  "Git" => "devicon-git-plain",
+  "HTML5" => "devicon-html5-plain-wordmark",
+  "CSS3" => "devicon-css3-plain-wordmark",
+  );
+?>
+
+<div id="skills" class="container">
+<?php
+foreach ($skills as $name => $class) {
+  echo '<div class="col-lg-3 col-md-3 col-sm-4">';
+  echo '<i class="' . $class . '"></i><br/><br/>';
+  echo $name;
+  echo '</div>';
+}
+?>
 </div>
 
 <div class="footer">
